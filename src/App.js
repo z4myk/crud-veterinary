642a19d0978330c61ca30bf30db5a18bfadebd65
Sebastian Mosquera <sebimosquera@gmail.com>
2021-11-22 16:51:03 -0300

    se agrego la logica para almacenar los pacientes en el array y el boton de eliminar para remover el turno asignado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [consulta, setConsulta] = useState({
+    nombre: "",
+    apellido: "",
+    informacion: "",
+    id: "",
+  });
+
+  const [consultas, setConsultas] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setConsulta({ ...consulta, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setConsultas([...consultas, consulta]);
+    setConsulta({
+      nombre: "",
+      apellido: "",
+      informacion: "",
+      id: "",
+    });
+    e.target.reset()
+  };
+
+  const eliminarConsulta = (id) => {
+    let consultaFiltrada = consultas.filter(item => item.id !== id);
+    setConsultas(consultaFiltrada)
+    console.log(id)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className=" row container">
+        <div className="col-md-6">
+          <h2 className="text-center my-4">Formulario de turnos</h2>
+          <hr />
+          <form onSubmit={handleSubmit}>
+            <label className="my-2">Nombre:</label>
+            <input
+              className="form-control w-75"
+              type="text"
+              placeholder="nombre"
+              name="nombre"
+              onChange={handleChange}
+            />
+            <label className="my-2">Apellido:</label>
+            <input
+              className="form-control w-75"
+              type="text"
+              placeholder="apellido"
+              name="apellido"
+              onChange={handleChange}
+            />
+            <label className="my-2">Datos del paciente:</label>
+            <input
+              className="form-control w-75"
+              type="text"
+              placeholder="informacion"
+              name="informacion"
+              onChange={handleChange}
+            />
+            <label className="my-2">Numero:</label>
+            <input
+              className="form-control w-75"
+              type="number"
+              placeholder="1"
+              name="id"
+              onChange={handleChange}
+            />
+            <button className="btn btn-success w-75 my-2" type="submit">
+              Enviar
+            </button>
+          </form>
+        </div>
+        <div className="col-md-6">
+          <h2 className="text-center my-4">Turnos</h2>
+          <hr />
+          {consultas.map((item) => (
+            <div key={item.id} className="mb-5">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title text-center">
+                    Nombre: {item.nombre}
+                  </h5>
+                  <hr />
+                  <h6 className="card-subtitle mb-2 text-muted text-center">
+                    Apellido: {item.apellido}
+                  </h6>
+                  <p className="card-text text-center">
+                    Información: {item.informacion}
+                  </p>
+                  <button className="btn btn-danger" onClick={(id) => eliminarConsulta(item.id)}>
+                    Eliminar
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
